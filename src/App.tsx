b52eccd0e4626a0002878bd7ac65f9f12cfed148
Mahmoud Fares <mahmoudfaresce@gmail.com>
2025-03-10 07:06:04 +0200

    vite & tailwind

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        Submit
+      </button>
+    </>
+  );
+}
+
+export default App;
